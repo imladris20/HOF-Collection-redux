93f009aabe7613ab9aae40597348dba3db8ee34e
Po Lien Lin <polien.joe.lin@gmail.com>
2023-06-28 19:47:38 +0800

    add the card component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Card from './Card.js'
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
+    <Card />
   </React.StrictMode>
 );
 
