35ea5730db7d6d38f5ff8191399fea8e3262a8d3
Po Lien Lin <polien.joe.lin@gmail.com>
2023-06-29 11:37:11 +0800

    Make Card as Cardlist's component and turned the index.js neater.

--- src/Card.js
+++ src/Card.js
@@ -18,7 +18,7 @@ const Card = (props) => {
     const {name,  backnumber, team, position} = props;
     return (
         <div className="tc bg-light-red dib br3 pa3 ma2 dim bw2 shadow-5">
-            <img alt="Chris Carpenter" src={`https://robohash.org/set_set4/${name}${backnumber}?size=200x200`} />
+            <img alt={`${name}`} src={`https://robohash.org/set_set4/${name}${backnumber}?size=200x200`} />
             <div>
                 <h2>{name}</h2>
                 <p>{`#${backnumber} in ${team}`}</p>
